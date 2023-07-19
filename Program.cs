733849d8dafbef865e7dca9aecf5b0bbd5ff3a07
Aleksey <kukoba.alexey@gmail.com>
2023-07-19 11:31:22 +0300

    final

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message) //Пишем передаваемое сообщение - запрашиваем у пользователя число - на выходе выдаем это число
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int number = Prompt("Введите число: ");
+Console.WriteLine($"Сумма всех цифр в числе {number} = {SumAllDigit(number)}");
